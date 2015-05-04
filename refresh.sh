a064e7eeb255a8196bfe4acdc627711f36a123e6
Chris Jerdonek <chris.jerdonek@gmail.com>
2015-05-03 21:45:54 -0700

    Adjust refresh.sh to copy the license file.

--- refresh.sh
+++ refresh.sh
@@ -1,6 +1,8 @@
 cp ../sf-base-election-data/_build/html/*.html .
 cp ../sf-base-election-data/_build/html/css/*.css css/
 cp ../sf-base-election-data/_build/html/data/*.json data/
+# Copy the LICENSE file.
+cp ../sf-base-election-data/_build/html/data/*.txt data/
 git add -u
 git commit -m "Refresh from master."
 git push
