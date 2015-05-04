374ee1c4430feb263d9097ede1d58099bb7c806c
Chris Jerdonek <chris.jerdonek@gmail.com>
2015-05-03 18:23:22 -0700

    Refresh from master.

--- refresh.sh
+++ refresh.sh
@@ -1,4 +1,5 @@
 cp ../sf-base-election-data/_build/html/*.html .
+cp ../sf-base-election-data/_build/html/css/*.css css/
 cp ../sf-base-election-data/_build/html/data/*.json data/
 git add -u
 git commit -m "Refresh from master."
